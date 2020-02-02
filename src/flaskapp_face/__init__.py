e14b0542b5b18661308d0b934ac3c5e1f3181b23
yeonchan choi <ycice@naver.com>
2020-02-02 12:06:02 +0900

    사진 저장위치 변경

--- src/flaskapp_face/__init__.py
+++ src/flaskapp_face/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+USER_IMG_DIR = os.path.join(os.path.dirname(__file__), 'user_imgs')
+
 
 import flaskapp_face.views
--- src/flaskapp_face/face_outline.py
+++ src/flaskapp_face/face_outline.py
@@ -2,20 +2,19 @@ import os
 import requests
 from uuid import uuid4
 from PIL import Image
+from flaskapp_face import USER_IMG_DIR
 
 
 def find_face(file_name):
-    STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
-
     naver_url = "https://openapi.naver.com/v1/vision/face"
     headers = {'X-Naver-Client-Id': '552ZS6v_QNFGoGORaPaD', 'X-Naver-Client-Secret': 'aLtCyh2VbL'}
-    files = {'image': open(os.path.join(STATIC_DIR, f'{file_name}.jpg'), 'rb')}
+    files = {'image': open(os.path.join(USER_IMG_DIR, f'{file_name}.jpg'), 'rb')}
 
     response = requests.post(url=naver_url, files=files, headers=headers)
     assert response.status_code // 100 == 2
 
     people_info = response.json()['faces']
-    img: Image.Image = Image.open(os.path.join(STATIC_DIR, f'{file_name}.jpg'))
+    img: Image.Image = Image.open(os.path.join(USER_IMG_DIR, f'{file_name}.jpg'))
 
     uuid_filenames = []
     for person in people_info:
@@ -25,7 +24,7 @@ def find_face(file_name):
         face_resized = face_img.resize((400, 400), resample=4)
 
         uuid_filename = str(uuid4())
-        face_resized.save(os.path.join(STATIC_DIR, f'face_{uuid_filename}.jpg'))
+        face_resized.save(os.path.join(USER_IMG_DIR, f'face_{uuid_filename}.jpg'))
         uuid_filenames.append(uuid_filename)
 
     return uuid_filenames
